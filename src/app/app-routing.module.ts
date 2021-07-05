c72e7b712d8682c17b53a47076063c37a23afd62
CelesteRenteria <celeste.renteria.is@unipolidgo.edu.mx>
2021-07-05 01:32:20 -0500

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'login', component:LoginComponent},
+  { path: 'register',component:RegisterComponent},
   { path: '', pathMatch: 'full', redirectTo: '/home' },
   { path: '**', component: NotfoundComponent }
 ]
